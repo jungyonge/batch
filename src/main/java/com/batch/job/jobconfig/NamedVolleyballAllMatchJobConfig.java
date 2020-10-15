ac4622b7f98dc1ce855396b8ebd91a9e7470f2be
Alphonse <jungyongchoi@coocha.com>
2020-10-15 16:24:43 +0900

    배구추가

---
+++ src/main/java/com/batch/job/jobconfig/NamedVolleyballAllMatchJobConfig.java
@@ -0,0 +1,70 @@
+package com.batch.job.jobconfig;
+
+import com.batch.job.listener.JobCompletionNotificationListener;
+import com.batch.job.task.processor.NamedBasketballAllMatchProcessor;
+import com.batch.job.task.reader.DummyReader;
+import com.batch.job.task.writer.NamedBasketballAllMatchWriter;
+import com.batch.model.BasketballModel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+
+@EnableBatchProcessing
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@Controller
+public class NamedVolleyballAllMatchJobConfig {
+
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    private JobCompletionNotificationListener notificationListener;
+    private DummyReader dummyReader;
+    private NamedBasketballAllMatchProcessor namedBasketballAllMatchProcessor;
+    private NamedBasketballAllMatchWriter namedBasketballAllMatchWriter;
+
+
+    @Autowired
+    public NamedVolleyballAllMatchJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory,
+                                            JobCompletionNotificationListener notificationListener, DummyReader dummyReader, NamedBasketballAllMatchProcessor namedBasketballAllMatchProcessor, NamedBasketballAllMatchWriter namedBasketballAllMatchWriter) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.notificationListener = notificationListener;
+        this.dummyReader = dummyReader;
+        this.namedBasketballAllMatchProcessor = namedBasketballAllMatchProcessor;
+        this.namedBasketballAllMatchWriter = namedBasketballAllMatchWriter;
+    }
+
+    @Bean
+    public Job namedBasketballAllMatchJob(){
+        return jobBuilderFactory.get("namedVolleyballAllMatchJob")
+                .preventRestart()
+                .listener(notificationListener)
+                .flow(namedBasketballAllMatchStep())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step namedBasketballAllMatchStep(){
+        return stepBuilderFactory.get("namedVolleyballAllMatchStep")
+                .<String, List<BasketballModel>> chunk(1)
+                .reader(dummyReader)
+                .processor(namedBasketballAllMatchProcessor)
+                .writer(namedBasketballAllMatchWriter)
+                .build();
+    }
+}
