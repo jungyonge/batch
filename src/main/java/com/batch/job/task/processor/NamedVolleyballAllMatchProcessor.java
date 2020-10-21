11a7d41471fe9e076bd989ab366a15c065b71784
Alphonse <jungyongchoi@coocha.com>
2020-10-21 16:58:07 +0900

    배구 라이브스코어로 변경

--- src/main/java/com/batch/job/task/processor/NamedVolleyballAllMatchProcessor.java
+++ src/main/java/com/batch/job/task/processor/NamedVolleyballAllMatchProcessor.java
@@ -58,7 +58,7 @@ public class NamedVolleyballAllMatchProcessor implements ItemProcessor<String, L
             Calendar cal = Calendar.getInstance();
             cal.setTime(new Date());
 
-            cal.set(2020, 9, 24);
+            cal.set(2020, 9, 10);
 
             DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -105,14 +105,10 @@ public class NamedVolleyballAllMatchProcessor implements ItemProcessor<String, L
                         aTeamModel.setLeague(getDivision(aTeamModel.getBTeam()));
                         bTeamModel.setLeague(getDivision(aTeamModel.getBTeam()));
 
-//                        if (!aTeamModel.getATeam().equals("") && !aTeamModel.getGameId().equals("")){
-                        log.info(aTeamModel.toString());
-                        log.info(bTeamModel.toString());
-
+                        if (!aTeamModel.getATeam().equals("") && !aTeamModel.getGameId().equals("")){
                             volleyballModelList.add(aTeamModel);
                             volleyballModelList.add(bTeamModel);
-
-//                        }
+                        }
                     }
 
                 }
