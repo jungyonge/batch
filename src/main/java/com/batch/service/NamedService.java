0a6cecc80ab8ee07feee110bbf0be1e8dc9e4793
jungyonge <jungyong_e@naver.com>
2023-06-18 14:31:01 +0900

    네임드 service 추가

--- src/main/java/com/batch/service/NamedService.java
+++ src/main/java/com/batch/service/NamedService.java
@@ -1,9 +1,15 @@
 package com.batch.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class NamedService {
+
+    private final RestTemplate restTemplate;
+
 
 
 }
